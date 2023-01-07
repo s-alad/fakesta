f9f46624303691951598c13ed8bfde01d597a46c
s-alad <n7.saad@gmail.com>
2023-01-07 01:47:28 -0500

    ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/navbar/navbar';
 
 function App() {
   return (
-    <></>
+    <>
+      <Navbar></Navbar>
+      <div className='app'>
+        <div className='punchline'>
+          <div className='fakesta'>FakeSta</div>
+          <div className='info'>
+            Find out who doesn't follow you back. Check out who someone started following or unfollowed. And much more!
+          </div>
+        </div>
+
+        <div className='public'>
+          <div className='public-title'>Enter the username of the account you would like to check for people that don't follow back.</div>
+          <div className='public-input'>
+            <input placeholder='@username'></input>
+            <button>Check!</button>
+          </div>
+        </div>
+
+        <div className='private'>
+          <div className='private-title'>Have a private account? Click here for more instructions</div>
+          <div className='private-subtitle'>Don't worry, you won't need to login to your account.</div>
+          <button>Private Check</button>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ client/src/components/navbar/navbar.tsx
@@ -0,0 +1,9 @@
+import react from 'react';
+import './navbar.css';
+
+export default function Navbar() {
+    return (
+        <div className='navbar'>
+        </div>
+    )
+}
