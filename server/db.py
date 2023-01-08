6e257e39b4437a3b077962075d5d99679b3b2bf1
s-alad <n7.saad@gmail.com>
2023-01-07 23:18:58 -0500

    stack crud

--- server/db.py
+++ server/db.py
@@ -1,7 +1,6 @@
 from dotenv import load_dotenv
 import os
 from supabase import create_client, Client
-
 from bot import Bot
 
 load_dotenv()
@@ -36,10 +35,21 @@ class BotBase:
         print('DELETING...')
         return self.supabase.table(self.table).delete().eq('id', id).execute()
 
+    def deletefirstbot(self):
+        print('DELETING FIRST...')
+        return self.supabase.table(self.table).delete().eq('id', self.getfirstbot().data[0]['id']).execute()
+
+    def deletelastbot(self):
+        print('DELETING LAST...')
+        return self.supabase.table(self.table).delete().eq('id', self.getlastbot().data[0]['id']).execute()
 
 if __name__ == '__main__':
     botbase = BotBase()
     print(botbase.find())
     print(botbase.getfirstbot())
     print(botbase.getlastbot())
+    #print(botbase.insertbot('test', 'test'))
+    #print(botbase.find())
+    print(botbase.deletelastbot())
+    print(botbase.find())
 
