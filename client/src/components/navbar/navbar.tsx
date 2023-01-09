0263b51ffdeb901f17d81651761e467b6082de22
s-alad <n7.saad@gmail.com>
2023-01-09 00:55:03 -0500

    navbar

--- client/src/components/navbar/navbar.tsx
+++ client/src/components/navbar/navbar.tsx
@@ -5,6 +5,13 @@ export default function Navbar() {
     return (
         <div className='navbar'>
             <div className='fakesta-title'>FakeSta</div>
+            <div className='link-items'>
+                <div className='link-item'>Fake Followers</div>
+                <div className='link-item'>Unfollow Tracker</div>
+                <div className='link-item'>Ex Snooper</div>
+                <div className='link-item'>About</div>
+
+            </div>
             <div className='nav-items'>
                 <div className='premium'>Premium</div>
                 <div className='login'>Login</div>
