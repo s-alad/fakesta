b6bc9bdba20d157c8fcdf5ae1218d7ac90cfd275
s-alad <n7.saad@gmail.com>
2023-01-09 00:49:18 -0500

    header

--- client/src/components/navbar/navbar.tsx
+++ client/src/components/navbar/navbar.tsx
@@ -4,6 +4,11 @@ import './navbar.css';
 export default function Navbar() {
     return (
         <div className='navbar'>
+            <div className='fakesta-title'>FakeSta</div>
+            <div className='nav-items'>
+                <div className='premium'>Premium</div>
+                <div className='login'>Login</div>
+            </div>
         </div>
     )
 }
